9a893a9dc82e05a01a5042a44c5e061c521ac1c2
susanjacke24-dev <susanjacke.24@gmail.com>
2026-02-20 12:58:32 -0600

    feat: implement layout with Flowbite and dynamic sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,3 @@
 <x-admin-layout>
-  Hola desde el panel de control del sistema de citas medicasn
+  Hola desde el panel de control del sistema de citas médicas
 </x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,86 +16,17 @@
 
         <!-- Styles -->
         @livewireStyles
+         <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bd-gray-50">
       
 @include('layouts.includes.Admin.navigation')
 @include('layouts.includes.Admin.sidebar')
 
 <div class="p-4 sm:ml-64 mt-14">
-   <div class="p-4 border-1 border-default border-dashed rounded-base">
-      <div class="grid grid-cols-3 gap-4 mb-4">
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-      </div>
-      <div class="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
-         <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-         </p>
-      </div>
-      <div class="grid grid-cols-2 gap-4 mb-4">
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-      </div>
-      <div class="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
-         <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-         </p>
-      </div>
-      <div class="grid grid-cols-2 gap-4">
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-         <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-            <p class="text-fg-disabled">
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-            </p>
-         </div>
-      </div>
-   </div>
+   <div Class="mt-14">
+      {{$slot}}
+   </div> 
 </div>
 
         @stack('modals')
